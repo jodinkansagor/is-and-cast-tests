40818e91639589c97772d837e0764e39e6edf109
Jodi N Kansagor <joedvice@gmail.com>
2019-12-02 15:59:10 -0800

    deleted potential issue from travis file to pass travis

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const { isNumber } = require('./lib/types.js');
+const { isNumber, isString, isBoolean, isArray, isObject, isFunction } = require('./lib/types.js');
 
 console.log(isNumber('3'));
+console.log(isString('hi'));
+console.log(isBoolean(true));
+console.log(isArray([3, 2]));
+console.log(isObject({ name: 'JBJ', age: 41 }));
+console.log(isFunction(() => {}));
+
